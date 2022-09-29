3916bc96f2afe4e238fd51ae55bdbdb8e74ff7e0
Joshua Quinones (RIT Student) <jiq4449@rit.edu>
2022-09-29 15:25:11 -0400

    Begun implementation of ring

--- Steel Heel Jam/Assets/Dev/Objects/Ring.cs
+++ Steel Heel Jam/Assets/Dev/Objects/Ring.cs
@@ -4,20 +4,35 @@ using UnityEngine;
 
 public class Ring : MonoBehaviour
 {
-    float startingRadius = 30.0f;
-    float currenRadius = 30.0f;
+    [SerializeField] public float startingRadius = 30.0f;
+    private float radius;
 
-    private Transform effTransform;
+    private float resizeTimer;
 
     // Start is called before the first frame update
     void Start()
     {
-        effTransform = transform;
+        radius = startingRadius;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (resizeTimer > 0)
+        {
+            float size = ((radius * 2) - transform.localScale.x) * Time.deltaTime;
+            transform.localScale = new Vector3(size, transform.localScale.y, size);
+        }
+    }
+
+    /// <summary>
+    /// Resizes the ring to the specified size.
+    /// </summary>
+    /// <param name="radius">The new radius of the ring.</param>
+    /// <param name="time">The time it takes for the ring to fully resize.</param>
+    public void ResizeRing(float radius, float time)
+    {
+        this.radius = radius;
+        resizeTimer = time;
     }
 }
