6f15154d9db4a6821aee2011b21e752208eb8a9b
Nick Baker <nickmbaker1@gmail.com>
2022-09-30 03:22:17 -0400

    Made players spawn in on the Demo gameplay scene. Added customizable spawn point locations and made the game randomly order them when spawning players.

--- Steel Heel Jam/Assets/Dev/Managers/GameManager.cs
+++ Steel Heel Jam/Assets/Dev/Managers/GameManager.cs
@@ -6,16 +6,64 @@ public class GameManager : MonoBehaviour
 {
     public AudioManager audioManager;
 
+    public GameSceneSettings gameSceneSettings;
+
+    public GameObject playerPrefab;
+
+    public PlayerStatus[] playerStatuses;
+
+
     // Start is called before the first frame update
     void Start()
     {
         audioManager = FindObjectOfType<AudioManager>();
+        SpawnPlayerPrefabs();
         //audioManager.Play("Smack");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void SpawnPlayerPrefabs()
+    {
+        Transform spawnPoints = gameSceneSettings.transform.GetChild(Mathf.Max(0, AppManager.app.TokenAmount - 2));
+        spawnPoints.gameObject.SetActive(true);
+        int[] spawnPointOrder = new int[spawnPoints.childCount];
+
+        for(int i = 0; i < spawnPointOrder.Length; i++){
+            spawnPointOrder[i] = i;
+        }
+
+        ShuffleArray(spawnPointOrder);
+
+        int j = 0;
+
+        for (int i = 0; i < AppManager.app.playerTokens.Length; i++)
+        {
+            if (AppManager.app.playerTokens[i] != null)
+            {
+                PlayerToken token = AppManager.app.playerTokens[i];
+                token.input.SwitchCurrentActionMap("Player");
+
+                GameObject player = Instantiate(playerPrefab, spawnPoints.GetChild(spawnPointOrder[j]).position, spawnPoints.GetChild(spawnPointOrder[j]).rotation);
+                token.SetUpPlayerPrefab(player);
+                j++;
+            }
+        }
+    }
+
+    private void ShuffleArray<T>(T[] array)
+    {
+        int n = array.Length;
+        while (n > 1)
+        {
+            int k = Random.Range(0, n--);
+            T temp = array[n];
+            array[n] = array[k];
+            array[k] = temp;
+        }
     }
 }
---
+++ Steel Heel Jam/Assets/Dev/Managers/GameSceneSettings.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSceneSettings : MonoBehaviour
+{
+
+    //this script will contain the references to the specific player spawn locations
+    //and item spawn locations
+
+    public Transform playerSpawnsFor2;
+    public Transform playerSpawnsFor3;
+    public Transform playerSpawnsFor4;
+    public Transform playerSpawnsFor5;
+    public Transform playerSpawnsFor6;
+    public Transform playerSpawnsFor7;
+    public Transform playerSpawnsFor8;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
