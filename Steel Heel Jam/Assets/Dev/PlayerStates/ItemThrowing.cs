e0151ce94094211d0eb6f5caff7165c5c8a6a74c
Brian Kiss <bjk4726@rit.edu>
2022-10-03 10:17:22 -0400

    Created ItemThrowing status, need throwing functioning first.

---
+++ Steel Heel Jam/Assets/Dev/PlayerStates/ItemThrowing.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class ItemThrowing : BasicState
+{
+    public ItemThrowing()
+    {
+        timeToChangeState = 999f; // Only change state on button release.
+        canPlayerControlMove = true;
+        canPlayerControlRotate = true;
+        updateMovement = true;
+        canAttack = false;
+        canDodgeRoll = true;
+        canBlock = true;
+        moveSpeedMultiplier = 1.0f;
+        //animationState = AnimationState.Throwing;
+        //visual = VisualChild.Throw;
+        canPickUp = false;
+
+        stateToChangeTo = new Idle();
+    }
+
+    public override void Update(PlayerStatus status)
+    {
+        base.Update(status);
+
+        // Decrease movespeed to a cap. (2 seconds to lerp from 1.0 to 0.2 movespeed.)
+        moveSpeedMultiplier = Mathf.Lerp(1.0f, 0.2f, timeInThisState / 2.0f);
+
+        status.movement.SetVelocityToMoveSpeedTimesFowardDirection();
+
+        // Needs a way to see when the input is released to change to Idle state.
+        if (/*input is released*/ true)
+        {
+            status.SetPlayerStateImmediately(new Idle());
+        }
+    }
+
+    public override void OnEnterThisState(BasicState prevState, PlayerStatus status)
+    {
+        base.OnEnterThisState(prevState, status);
+
+        status.movement.velocity = status.movement.velocity.normalized * status.movement.CurrentMoveSpeed;
+    }
+}
