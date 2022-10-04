2d3d060f6ead383346cb7c68a66a3bf8cc2b5d30
Brian Kiss <bjk4726@rit.edu>
2022-10-04 09:54:03 -0400

    Moved itemIndicators over to HUDManager script

--- Steel Heel Jam/Assets/Dev/Managers/HUDManager.cs
+++ Steel Heel Jam/Assets/Dev/Managers/HUDManager.cs
@@ -13,6 +13,10 @@ public class HUDManager : MonoBehaviour
 
     public List<PlayerHeader> headers;
 
+    private ItemManager itemManagerScript;
+    //public GameObject arrowPrefab;
+    //public List<GameObject> arrows;
+
 
     public void CreatePlayerHeader(PlayerStatus status)
     {
@@ -44,11 +48,44 @@ public class HUDManager : MonoBehaviour
     void Start()
     {
         canvas = GetComponent<Canvas>();
+        // Since both are prefabs...
+        itemManagerScript = GameObject.FindGameObjectWithTag("ItemManager").GetComponent<ItemManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        for (int i = 0; i < itemManagerScript.itemsOnGround.Count; i++)
+        {
+            if (!itemManagerScript.itemsOnGround[i].GetComponent<Item>().isOnScreen)
+            {
+                // Create a new GUI arrow.
 
+
+                // Calculate direction to point arrow.
+                //Vector3 itemVector = NormalVectorToItem(i);
+                //Debug.Log("Normal direction is: " + itemVector.x + ", " + itemVector.z);
+
+                // Calculate edge of screen.
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// Calculates the normal vec3 from the middle of the camera projection to the item.
+    /// </summary>
+    /// <returns>Normalized vector 3 pointing towards the item.</returns>
+    private Vector3 NormalVectorToItem(int i)
+    {
+        var ray = Camera.main.ScreenPointToRay(new Vector2(Screen.height / 2, Screen.width / 2));
+        RaycastHit hitPoint;
+
+        if (Physics.Raycast(ray, out hitPoint, 100.0f))
+        {
+            return (hitPoint.point - itemManagerScript.itemsOnGround[i].transform.position).normalized;
+        }
+
+        return new Vector3();
     }
 }
