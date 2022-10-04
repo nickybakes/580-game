5802c02f7e3b0d34bbcda12277f45b07de72c8ff
Brian Kiss <bjk4726@rit.edu>
2022-10-04 09:45:42 -0400

    Got vec math to find normal to item.
    Commented out for now.
    
    Doesn't create a visual indicator, not sure best place to put it.

--- Steel Heel Jam/Assets/Dev/Objects/ItemIndicators.cs
+++ Steel Heel Jam/Assets/Dev/Objects/ItemIndicators.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ItemIndicators : MonoBehaviour
@@ -16,26 +17,37 @@ public class ItemIndicators : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //for (int i = 0; i < itemManagerScript.itemsOnGround.Count; i++)
-        //{
-        //    if (!itemManagerScript.itemsOnGround[i].GetComponent<Item>().isOnScreen)
-        //    {
-        //        // Create a new GUI arrow.
+        for (int i = 0; i < itemManagerScript.itemsOnGround.Count; i++)
+        {
+            if (!itemManagerScript.itemsOnGround[i].GetComponent<Item>().isOnScreen)
+            {
+                // Create a new GUI arrow.
 
-        //        // Calculate direction to point arrow.
-        //        NormalVectorToItem();
 
-        //        // Calculate edge of screen.
-        //    }
-        //}
+                // Calculate direction to point arrow.
+                //Vector3 itemVector = NormalVectorToItem(i);
+                //Debug.Log("Normal direction is: " + itemVector.x + ", " + itemVector.z);
+
+                // Calculate edge of screen.
+            }
+        }
     }
+
 
     /// <summary>
     /// Calculates the normal vec3 from the middle of the camera projection to the item.
     /// </summary>
     /// <returns>Normalized vector 3 pointing towards the item.</returns>
-    private Vector3 NormalVectorToItem()
+    private Vector3 NormalVectorToItem(int i)
     {
+        var ray = Camera.main.ScreenPointToRay(new Vector2(Screen.height / 2, Screen.width / 2));
+        RaycastHit hitPoint;
+
+        if (Physics.Raycast(ray, out hitPoint, 100.0f))
+        {
+            return (hitPoint.point - itemManagerScript.itemsOnGround[i].transform.position).normalized;
+        }
+
         return new Vector3();
     }
 }
