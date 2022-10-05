d6e3b3ece2f1ddfde50b03e9998634a706d37099
Brian Kiss <bjk4726@rit.edu>
2022-10-05 10:59:05 -0400

    Straight throwing works using unity Physics
    starting position of cube based on pickup area. Change to hand position in future.

--- Steel Heel Jam/Assets/Dev/Objects/ItemTrajectory.cs
+++ Steel Heel Jam/Assets/Dev/Objects/ItemTrajectory.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ItemTrajectory : MonoBehaviour
@@ -7,14 +8,12 @@ public class ItemTrajectory : MonoBehaviour
     public bool isTargetedThrow;
     public bool isThrown;
     public float chargeAmount;
-    private float chargeAmountMultiplier = 0.1f;
+    private float chargeAmountMultiplier = 20f;
 
     private bool wasThrown;
-    private Vector3 throwDirection;
-    private float gravity = -0.1f;
-    //private float height = 1.0f;
 
     private Transform tr;
+    private Rigidbody rb;
 
     public PlayerStatus target;
     public PlayerStatus thrower;
@@ -23,6 +22,7 @@ public class ItemTrajectory : MonoBehaviour
     void Start()
     {
         tr = transform;
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -30,40 +30,55 @@ public class ItemTrajectory : MonoBehaviour
     {
         if (isThrown)
         {
-            if (!wasThrown)
-                throwDirection = GetInitialDirection();
 
             if (isTargetedThrow)
             {
                 UpdateTargetedThrow();
+                isThrown = false;
             }
             else
             {
                 UpdateStraightThrow();
             }
 
+            // Set isThrown to false when it's "grounded" or being held.
+            if (rb.velocity.magnitude < 0.01f)
+            {
+                isThrown = false;
+                wasThrown = false;
+            }
         }
     }
 
     private void UpdateStraightThrow()
     {
 
+        if (!wasThrown)
+        {
+            wasThrown = true;
+
+            Vector3 initialDirection = thrower.movement.ActualFowardDirection;
+            initialDirection.x *= chargeAmount * chargeAmountMultiplier;
+            initialDirection.z *= chargeAmount * chargeAmountMultiplier;
+            initialDirection.y = 5 * chargeAmount; //chargeamt from 0-3
+
+            rb.velocity = new Vector3(0,0,0);
+            rb.AddForce(initialDirection, ForceMode.Impulse);
+        }
+
         //transform.position = thrower.movement.ActualFowardDirection * chargeAmount;
         //Debug.Log(thrower.movement.ActualFowardDirection);
 
-        //height += gravity * Time.deltaTime;
+        //height -= Physics.gravity.y * Time.fixedDeltaTime;
 
-        tr.position = new Vector3(tr.position.x + (throwDirection.x * chargeAmount * chargeAmountMultiplier), tr.position.y/* + height*/, tr.position.z + (throwDirection.z * chargeAmount * chargeAmountMultiplier));
+        //tr.position = new Vector3(tr.position.x + (throwDirection.x * chargeAmount * chargeAmountMultiplier), tr.position.y, tr.position.z + (throwDirection.z * chargeAmount * chargeAmountMultiplier));
+
+        // If it collides with a surface, set isThrown to false.
+        //if ()
     }
 
     private void UpdateTargetedThrow()
     {
         
-    }
-
-    private Vector3 GetInitialDirection()
-    {
-        wasThrown = true;
-        return thrower.movement.ActualFowardDirection;
     }
 }
