ae75a6d07b9294a010cfb0f4f5b3629849138ed3
lanexl <56983001+lanexl@users.noreply.github.com>
2022-10-27 14:49:06 -0400

    Lead Pipe values

--- Steel Heel Jam/Assets/Dev/EquipStates/LeadPipe.cs
+++ Steel Heel Jam/Assets/Dev/EquipStates/LeadPipe.cs
@@ -14,41 +14,31 @@ public class LeadPipe : DefaultState
         combo = new Attack[]
         {
             new Attack( // *** 1st Hit ***
-                1.5f, // Damage Multiplier
-                1.2f, // Knockback Multiplier
+                1.7f, // Damage Multiplier
+                1.4f, // Knockback Multiplier
                 0.4f, // Knockback Height Multiplier
-                1.5f, // Hitstun Multiplier
+                1.3f, // Hitstun Multiplier
                 1.5f, // Radius Multiplier
                 1.0f, // Height Multiplier
-                1.0f, // Startup Multiplier
-                1.0f, // Duration Multiplier
-                1.0f, // Recovery Multiplier
-                1.0f  // Forward Speed Multiplier
+                1.5f, // Startup Multiplier
+                1.3f, // Duration Multiplier
+                1.2f, // Recovery Multiplier
+                1.4f,  // Forward Speed Multiplier
+                AttackAnimation.Swipe_02
                 ),
             new Attack( // *** 2nd Hit ***
-                1.8f, // Damage Multiplier
-                1.2f, // Knockback Multiplier
+                1.7f, // Damage Multiplier
+                1.4f, // Knockback Multiplier
                 0.4f, // Knockback Height Multiplier
-                1.0f, // Hitstun Multiplier
-                2.3f, // Radius Multiplier
+                1.3f, // Hitstun Multiplier
+                1.5f, // Radius Multiplier
                 1.0f, // Height Multiplier
-                0.7f, // Startup Multiplier
+                1.5f, // Startup Multiplier
                 1.3f, // Duration Multiplier
-                1.0f, // Recovery Multiplier
-                1.5f  // Forward Speed Multiplier
-                ),
-            new Attack( // *** 3rd Hit ***
-                2.0f, // Damage Multiplier
-                1.2f, // Knockback Multiplier
-                0.4f, // Knockback Height Multiplier
-                1.0f, // Hitstun Multiplier
-                2.9f, // Radius Multiplier
-                1.0f, // Height Multiplier
-                0.7f, // Startup Multiplier
-                1.6f, // Duration Multiplier
-                1.0f, // Recovery Multiplier
-                2.0f  // Forward Speed Multiplier
-                ),
+                1.2f, // Recovery Multiplier
+                1.4f,  // Forward Speed Multiplier
+                AttackAnimation.Swipe_01
+                )
         };
 
         maxComboCount = combo.Length;
