d051d4d99196c2ecdac6fd0a0dec1d1c624c018e
lanexl <56983001+lanexl@users.noreply.github.com>
2022-10-27 14:24:25 -0400

    Baseball Bat values

--- Steel Heel Jam/Assets/Dev/EquipStates/BaseballBat.cs
+++ Steel Heel Jam/Assets/Dev/EquipStates/BaseballBat.cs
@@ -14,41 +14,19 @@ public class BaseballBat : DefaultState
         combo = new Attack[]
         {
             new Attack( // *** 1st Hit ***
-                1.5f, // Damage Multiplier
-                1.2f, // Knockback Multiplier
-                0.4f, // Knockback Height Multiplier
+                2.0f, // Damage Multiplier
+                2.0f, // Knockback Multiplier
+                1.6f, // Knockback Height Multiplier
                 1.5f, // Hitstun Multiplier
                 1.5f, // Radius Multiplier
                 1.0f, // Height Multiplier
-                1.0f, // Startup Multiplier
-                1.0f, // Duration Multiplier
-                1.0f, // Recovery Multiplier
-                1.0f  // Forward Speed Multiplier
-                ),
-            new Attack( // *** 2nd Hit ***
-                1.8f, // Damage Multiplier
-                1.2f, // Knockback Multiplier
-                0.4f, // Knockback Height Multiplier
-                1.0f, // Hitstun Multiplier
-                2.3f, // Radius Multiplier
-                1.0f, // Height Multiplier
-                0.7f, // Startup Multiplier
+                2.0f, // Startup Multiplier
                 1.3f, // Duration Multiplier
                 1.0f, // Recovery Multiplier
-                1.5f  // Forward Speed Multiplier
-                ),
-            new Attack( // *** 3rd Hit ***
-                2.0f, // Damage Multiplier
-                1.2f, // Knockback Multiplier
-                0.4f, // Knockback Height Multiplier
-                1.0f, // Hitstun Multiplier
-                2.9f, // Radius Multiplier
-                1.0f, // Height Multiplier
-                0.7f, // Startup Multiplier
-                1.6f, // Duration Multiplier
-                1.0f, // Recovery Multiplier
-                2.0f  // Forward Speed Multiplier
-                ),
+                0.3f,  // Forward Speed Multiplier
+                AttackAnimation.Swipe_01,
+                AttackDirection.Horizontal
+                )
         };
 
         maxComboCount = combo.Length;
