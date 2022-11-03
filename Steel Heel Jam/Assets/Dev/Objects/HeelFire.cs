10a40b159f41abd9b88f8968f0b0b3f408cdcf00
Joshua Quinones (RIT Student) <jiq4449@rit.edu>
2022-11-03 14:14:37 -0400

    Introduced Heel Fire Mechanic

---
+++ Steel Heel Jam/Assets/Dev/Objects/HeelFire.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeelFire : MonoBehaviour
+{
+    [SerializeField] PlayerStatus playerStatus;
+    int playerNumber;
+    [SerializeField] float damage;
+    [SerializeField] float knockback;
+    [SerializeField] float knockbackHeight;
+    [SerializeField] float timeInKnockback;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerNumber = playerStatus.playerNumber;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == Tag.Player.ToString())
+        {
+            PlayerStatus player = other.GetComponent<PlayerStatus>();
+
+            if (playerNumber == player.PlayerNumber) return;
+
+            player.GetHitByElbowDrop(
+                playerStatus.transform.position,
+                player.transform.position,
+                damage,
+                knockback,
+                knockbackHeight,
+                timeInKnockback,
+                playerStatus
+                );
+        }
+    }
+}
