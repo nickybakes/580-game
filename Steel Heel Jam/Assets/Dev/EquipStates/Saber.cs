7d729561aa68d958ff5d11991c2a322d536efd7b
Brian Kiss (RIT Student) <bjk4726@rit.edu>
2022-11-03 14:04:31 -0400

    Red collision spheres disabled
    Updated saber hitboxes.

--- Steel Heel Jam/Assets/Dev/EquipStates/Saber.cs
+++ Steel Heel Jam/Assets/Dev/EquipStates/Saber.cs
@@ -19,38 +19,41 @@ public class Saber : DefaultState
                 0.4f, // Knockback Height Multiplier
                 1.5f, // Hitstun Multiplier
                 1.5f, // Radius Multiplier
-                1.0f, // Height Multiplier
+                5.0f, // Height Multiplier
                 1.0f, // Startup Multiplier
                 1.0f, // Duration Multiplier
                 1.0f, // Recovery Multiplier
                 1.0f,  // Forward Speed Multiplier
-                AttackAnimation.Swipe_01
+                AttackAnimation.Swipe_01,
+                AttackDirection.Forward
                 ),
             new Attack( // *** 2nd Hit ***
                 1.8f, // Damage Multiplier
                 1.2f, // Knockback Multiplier
                 0.4f, // Knockback Height Multiplier
                 1.0f, // Hitstun Multiplier
-                2.3f, // Radius Multiplier
-                1.0f, // Height Multiplier
+                1.5f, // Radius Multiplier
+                5.0f, // Height Multiplier
                 0.7f, // Startup Multiplier
                 1.3f, // Duration Multiplier
                 1.0f, // Recovery Multiplier
                 1.5f,  // Forward Speed Multiplier
-                AttackAnimation.Swipe_02
+                AttackAnimation.Swipe_02,
+                AttackDirection.Forward
                 ),
             new Attack( // *** 3rd Hit ***
                 2.0f, // Damage Multiplier
                 1.2f, // Knockback Multiplier
                 0.4f, // Knockback Height Multiplier
                 1.0f, // Hitstun Multiplier
-                2.9f, // Radius Multiplier
-                1.0f, // Height Multiplier
+                2.0f, // Radius Multiplier
+                10.0f, // Height Multiplier
                 0.7f, // Startup Multiplier
                 1.6f, // Duration Multiplier
                 1.0f, // Recovery Multiplier
                 2.0f,  // Forward Speed Multiplier
-                AttackAnimation.Stab_01
+                AttackAnimation.Stab_01,
+                AttackDirection.Forward
                 ),
         };
 
