9a1029cb75425ed43e28dd9fb3c3afab6724f937
Nick Baker <nickmbaker1@gmail.com>
2022-11-17 09:16:36 -0500

    Started getting the displays for the characters in the menu

--- Steel Heel Jam/Assets/Dev/Managers/MenuManager.cs
+++ Steel Heel Jam/Assets/Dev/Managers/MenuManager.cs
@@ -10,7 +10,7 @@ public class MenuManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Shader.SetGlobalFloat("ringRadius", 3000);
     }
 
     // Update is called once per frame
